330d16922dbd2b91d917b7bfbb15f55b6eb2f693
ST10535608 <ST10535608@myemeris.edu.za>
2026-06-09 14:00:44 +0200

    final touches

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,16 +4,12 @@ plugins {
 
 android {
     namespace = "com.makayla.drivesafetracker"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.makayla.drivesafetracker"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/makayla/drivesafetracker/DetailsScreen.kt
+++ app/src/main/java/com/makayla/drivesafetracker/DetailsScreen.kt
@@ -18,11 +18,33 @@ class DetailsScreen : AppCompatActivity() {
 
         val txtResults = findViewById<TextView>(R.id.txtResults)
         val btnBack = findViewById<Button>(R.id.btnBack)
+        val txtHoursTotal = findViewById<TextView>(R.id.txtHoursTotal)
+        val txtHighestDriving = findViewById<TextView>(R.id.txtHighestDriving)
+        val txtAvgSleep = findViewById<TextView>(R.id.txtAvgSleep)
+        val txtFeedback = findViewById<TextView>(R.id.txtFeedback)
 
         val bundle = intent.extras
         val total = bundle?.getInt("total") ?: 0
-        val highestDriven = bundle?.getInt("highest")?: 0
-        val averageSleep = bundle?.getInt("average")?: 0
+        val highestDriven = bundle?.getString("highest")?: ""
+        val averageSleep = bundle?.getDouble("average")?: 0.0
+
+        txtHoursTotal.text = "Total hours driven: $total hrs"
+        txtHighestDriving.text = "Most driving: $highestDriven"
+        val avgFormatted = String.format("%1f", averageSleep)
+        txtAvgSleep.text = "Average sleep: $avgFormatted hrs"
+
+        if (total > 40) {
+            txtFeedback.text = "You are driving too much. Take regular breaks."
+        } else if (total >= 20) {
+            txtFeedback.text = "Reasonable week. Keep monitoring your rest stops."
+        } else {
+            txtFeedback.text = "Great job! You are managing your driving safely."
+        }
+
+        // Back button
+        btnBack.setOnClickListener {
+            finish()
+        }
 
     }
 }
